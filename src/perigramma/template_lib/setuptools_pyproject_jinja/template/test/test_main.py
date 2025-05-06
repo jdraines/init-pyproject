3127e752157ac73cc888d2387ff1d7925f0c4b4b
John Raines <johndanielraines@gmail.com>
2025-05-06 15:07:13 -0500

    deleted the pystring template for the setuptools pyproject

--- src/perigramma/template_lib/setuptools_pyproject_jinja/template/test/test_main.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import patch
-
-from {{ project_name }}.main import main
-
-@patch("{{project_name}}.main.say_hello")
-def test_main(mock_say_hello):
-    main()
-    mock_say_hello.assert_called_once()
-
